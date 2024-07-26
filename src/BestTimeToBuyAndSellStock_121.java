b8030ec173714a4bfab385f3c666816cc00befb1
raheleh <rahelehfaryadras@gmail.com>
2024-07-26 12:42:40 +0330

    # problems - 121

--- src/BestTimeToBuyAndSellStock_121.java
+++ src/BestTimeToBuyAndSellStock_121.java
@@ -6,10 +6,25 @@ public class BestTimeToBuyAndSellStock_121 {
             if (prices[i] < candidate) {
                 candidate = prices[i];
             }
-            if (prices[i] - candidate > profit) {
+            else if (prices[i] - candidate > profit) {
                 profit = prices[i] - candidate;
             }
         }
         return profit;
+    }
+
+
+    public int maxProfit2(int[] prices) {
+        int min_val = Integer.MAX_VALUE;
+        int max_profit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i]<min_val) {
+                min_val = prices[i];
+            }
+            else if (prices[i] - min_val > max_profit) {
+                max_profit = prices[i] - min_val;
+            }
+        }
+        return max_profit;
     }
 }
