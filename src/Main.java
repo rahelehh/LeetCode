2a1e674f3ae21939c5166b4fea9de858343d2707
raheleh <rahelehfaryadras@gmail.com>
2024-07-26 10:55:22 +0330

    # problems - 189

--- src/Main.java
+++ src/Main.java
@@ -29,8 +29,19 @@ public class Main {
 //        System.out.println("****************** Remove Duplicate From Sorted Array ******************\n" + removeDuplicatesFromSortedArrayII.removeDuplicates(nums));
 
         //********************************** Majority Element **********************************
-        int nums[] = {2,2,3,5,6,2,2,3,6,2,2};
-        MajorityElement_169 majorityElement = new MajorityElement_169();
-        System.out.println("****************** Majority Element ******************\n" + majorityElement.majorityElement(nums));
+//        int nums[] = {2,2,3,5,6,2,2,3,6,2,2};
+//        MajorityElement_169 majorityElement = new MajorityElement_169();
+//        System.out.println("****************** Majority Element ******************\n" + majorityElement.majorityElement(nums));
+
+        //********************************** Rotate Array **********************************
+        int nums[] = {1,2,3,4,5,6,7};
+        int k =4;
+        k = k%nums.length;
+        RotateArray_189 rotateArray = new RotateArray_189();
+        System.out.println("****************** Rotate Array ******************\n");
+        rotateArray.rotate(nums,k);
+        for (int i=0;i<nums.length;i++) {
+            System.out.println(nums[i]);
+        }
     }
 }
