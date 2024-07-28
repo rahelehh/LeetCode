c66bd48cac6bc0a61a93b86e76a6c234d42f736a
raheleh <rahelehfaryadras@gmail.com>
2024-07-28 12:18:49 +0330

    # problems - 392

--- src/twoPointers/IsSubsequence_392.java
+++ src/twoPointers/IsSubsequence_392.java
@@ -3,17 +3,15 @@ package twoPointers;
 public class IsSubsequence_392 {
     public boolean isSubsequence(String s, String t) {
         int j = 0;
-        if(s.length()==0)
+        if (s.length() == 0)
             return true;
-        for (int i = 0; i < t.length();i++) {
-            if(t.charAt(i)  != s.charAt(j))
+        for (int i = 0; i < t.length(); i++) {
+            if (t.charAt(i) != s.charAt(j))
                 continue;
             j++;
-            if(j>=s.length())
-                break;
+            if (j == s.length())
+                return true;
         }
-        if(j==s.length())
-            return true;
         return false;
     }
 }
